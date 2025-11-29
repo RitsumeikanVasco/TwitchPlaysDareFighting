475a54493fe6881feecf6cd9598fd580ce9ad1ce
Prooheckcp <vasco.soares.2001@gmail.com>
2025-11-29 23:08:56 +0900

    Setting up message listener

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,40 @@
+/*
+
+To get the bot token just insert this into the browser and extract the access_token
+
+https://id.twitch.tv/oauth2/authorize?response_type=token&client_id=YOUR_CLIENT_ID&redirect_uri=http://localhost:3000&scope=chat:read+chat:edit
+
+*/
+
 require("dotenv").config();
 
 import express from 'express'
 import Config from "./Config.json"
 import API from "./API.json"
 import {getToken} from "./functions/getToken"
+import tmi from "tmi.js"
 
-interface TokenResponse {
-    access_token: string;
-    expires_in: number;
-    token_type: 'bearer';
-}
+// Types
+import type { TokenResponse } from './ChatTypes.js';
 
 const app = express()
 
 app.listen(Config.port, async () => {
     let token: TokenResponse = await getToken()
+    let accessToken: string = token.access_token
+    let channelToListenUsername: string = process.env.CHANNEL_NAME as string
 
+    const client = new tmi.Client({
+	    channels: [ channelToListenUsername ],
+        identity: {
+            username: process.env.BOT_USERNAME,
+            password: process.env.BOT_TOKEN
+        },        
+    })
+
+    client.connect()
+
+    client.on('message', (channel: string, tags: tmi.ChatUserstate, message: string, self: boolean) => {
+        //client.say(channel, `@${tags.username}, heya!`);
+    })
 })
