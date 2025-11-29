67ef364a4b92b6c6cdaf7b26338ea1efad8b1e0a
Prooheckcp <vasco.soares.2001@gmail.com>
2025-11-29 20:47:38 +0900

    Creating basic web server

--- src/index.ts
+++ src/index.ts
@@ -1,30 +1,14 @@
+require("dotenv").config();
 
-require("dotenv").config()
-const request = require("request")
+import express from 'express'
+import Config from "./Config.json"
+import API from "./API.json"
+import {getToken} from "./functions/getToken"
 
+const app = express()
 
-const getToken = (_url: any, callback: any) => {
-    const options = {
-        url: process.env.GET_TOKEN,
-        json: true,
-        body: {
-            client_id: process.env.CLIENT_ID,
-            client_secret: process.env.CLIENT_SECRET,
-            grant_type: 'client_credentials'
-        }
-    }
-
-    request.post(options, (err: any, res: any, body: any) => {
-        if (err)
-            return console.log(err);
-
-        console.log(`Status: ${res.status}`)
-        console.log(body)
-
-        callback(res)
-    })
-}
-
-getToken(process.env.GET_TOKEN, (res: any)=>{
-    console.log(res)
+app.listen(Config.port, () => {
+    let token = getToken()
+    
+    console.log(token)
 })
