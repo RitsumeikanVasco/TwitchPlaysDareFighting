3b47ebc8f0127da42a5c502d91a4e03679c61866
Prooheckcp <vasco.soares.2001@gmail.com>
2025-11-30 16:36:19 +0900

    Finished declaration

--- src/socket/index.ts
+++ src/socket/index.ts
@@ -2,16 +2,24 @@ import http from "http"
 import {Server} from "socket.io"
 import Config from "./../Config.json"
 
+const CONNECTION_MESSAGE: string = "Client connected"
+
+let io: Server
+
 export async function socketInit(){
-    const server = http.createServer();
-    const io = new Server(server);
+    const server = http.createServer()
+    io = new Server(server)
+
+    io.on("connection", (_socket) => {
+        console.log(CONNECTION_MESSAGE);
+    });
 
     //Initiate the server on the socket port
     server.listen(Config.socket_port, ()=>{
         console.log(`Listening on Port ${Config.socket_port}`)
     })
+}
 
-    io.on("connection", (socket) => {
-        console.log("Client connected");
-    });
+export async function getSocket(){
+    return io
 }
