74c090372e988a91441ec1af5dd55e632f80efc6
Prooheckcp <vasco.soares.2001@gmail.com>
2025-12-08 23:27:17 +0900

    Setting database values

--- src/Database/index.ts
+++ src/Database/index.ts
@@ -6,10 +6,9 @@ mongoose.connection.on("connected", () => {
     console.log("Connected to MongoDB Server ✅");
 })
 
-
 export async function databaseInit(){
     console.log("Database Init")
-    await mongoose.connect(`mongodb://127.0.0.1:${Config.database_port}`);
+    await mongoose.connect(`mongodb://127.0.0.1:${Config.database_port}/${Config.database_name}`);
     console.log("Connected to MongoDB")
 
     const playerDataModel = mongoose.model("PlayerData", PlayerData)
