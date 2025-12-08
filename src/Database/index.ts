949963a3386a807f26dfa5b24ccdabdeef7e9931
Prooheckcp <vasco.soares.2001@gmail.com>
2025-12-08 18:19:26 +0900

    Setup mongoDB server

--- src/Database/index.ts
+++ src/Database/index.ts
@@ -3,11 +3,14 @@ import PlayerData from "./schemas/PlayerData"
 import Config from "./../Config.json"
 
 mongoose.connection.on("connected", () => {
-    console.log("Mongoose connected to DB");
+    console.log("Connected to MongoDB Server ✅");
 })
 
+
 export async function databaseInit(){
+    console.log("Database Init")
     await mongoose.connect(`mongodb://127.0.0.1:${Config.database_port}`);
+    console.log("Connected to MongoDB")
 
     const playerDataModel = mongoose.model("PlayerData", PlayerData)
     let poop = new playerDataModel()
