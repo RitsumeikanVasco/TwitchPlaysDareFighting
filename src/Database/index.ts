49dacccea66e41e9088e9a4abfb7bd3f59e9cf0c
Prooheckcp <vasco.soares.2001@gmail.com>
2025-12-08 23:45:22 +0900

    Cleaning console.log

--- src/Database/index.ts
+++ src/Database/index.ts
@@ -11,7 +11,5 @@ let playerDataModel;
 export async function databaseInit(){
     console.log("Database Init")
     await mongoose.connect(`mongodb://127.0.0.1:${Config.database_port}/${Config.database_name}`);
-    console.log("Connected to MongoDB")
-
     playerDataModel = mongoose.model("PlayerData", PlayerData)
 } 
