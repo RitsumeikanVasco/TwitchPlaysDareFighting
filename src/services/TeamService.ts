c4a1572f658a3a1cd771c6d84f5d71891ec05880
Prooheckcp <vasco.soares.2001@gmail.com>
2026-01-04 21:33:03 +0900

    Creating TeamService

---
+++ src/services/TeamService.ts
@@ -0,0 +1,44 @@
+import Team from "./../enums/Team"
+import { EventEmitter } from "events";
+
+const PLAYER_TEAMS: Map<string, Team> = new Map()
+
+export let teamEmitter = new EventEmitter()
+
+export function getTeam(userid: string): Team | null{
+    return PLAYER_TEAMS.get(userid) || null
+}
+
+export function hasTeam(userid: string): boolean{
+    return getTeam(userid) != null
+}
+
+export function getPlayersFromTeam(team: Team): string[]{
+    let playerArray: string[] = []
+
+    PLAYER_TEAMS.forEach((value: Team, userid: string)=>{
+        if (team == value)
+            playerArray.push(userid)
+    })
+
+    return playerArray
+}
+
+export function getTeamCount(team: Team): number{
+    return getPlayersFromTeam(team).length
+}
+
+export function setTeam(userid: string, team: Team){
+    if (getTeam(userid) == team)
+        return
+
+    teamEmitter.emit("JoinedTeam", userid, team)
+}
+
+export function leaveTeam(userid: string){
+    if (!hasTeam(userid))
+        return
+
+    PLAYER_TEAMS.delete(userid)
+    teamEmitter.emit("LeftTeam", userid)
+}
